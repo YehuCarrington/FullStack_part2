cabb41d54c3a83ff5022142a18682e976034ba37
Yehu Carrington <yehucarrington@icloud.com>
2024-06-05 22:39:54 -0400

    Submission for part2 exercise2.1

---
+++ part2/src/App.jsx
@@ -0,0 +1,49 @@
+const Course = ({course}) =>{
+
+  let parts = course.parts.map((course,i) => (
+    <div key={i}>
+      <p>{course.name} {course.exercises}</p>
+    </div>
+  ))
+
+  return(
+    <div>
+      <h1>{course.name}</h1>
+      <>{parts}</>
+    </div>
+  )
+}
+
+const App = () => {
+  
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'State of Play',
+        exercises: 8,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

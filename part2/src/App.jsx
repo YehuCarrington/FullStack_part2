a9fa7d68533e9be8b3c0724cc5695f77c3671657
Yehu Carrington <yehucarrington@icloud.com>
2024-06-09 02:28:26 -0400

    Submission for part 2 exercise 2.8

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -4,7 +4,7 @@ function DisplayPhonebook({ persons }) {
   return persons.map((person, index) => {
     return (
       <li key={index}>
-        {person.name}
+        {person.name} {person.number}
       </li>
     )
   })
@@ -12,25 +12,41 @@ function DisplayPhonebook({ persons }) {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '1868-555-5555', id: 1 },
+    { name: 'Hadas Melix', number: '1868-777-7777', id: 2 },
+    { name: 'Elsa Gate', number: '1868-444-4444', id: 3 },
+    { name: 'Panda Trix', number: '1868-222-2222', id: 4 }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newId, setId] = useState(5)
 
   const handleNewName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNewNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   const updatePersons = (event) => {
     event.preventDefault()
-    const personObj = { name: newName }
+    const personObj = { name: newName, number: newNumber, id: newId }
 
-    //We can perform the check here, lets access the array of objects
-    const checkPersons = persons.map(person => person.name)
-    if (checkPersons.includes(newName)) { alert( `${newName} is already in the phonebook`) }
+    if ((newName.trim().length === 0) || (newNumber.trim().length === 0))
+      alert('Please enter a name AND a number')
     else {
-      setPersons(persons.concat(personObj))
-      setNewName('')
+      //We can perform the check here, lets access the array of objects
+      const checkPersons = persons.map(person => person.name)
+      if (checkPersons.includes(newName)) { alert(`${newName} is already in the phonebook`) }
+      else {
+        setPersons(persons.concat(personObj))
+        setNewName('')
+        setNewNumber('')
+        setId(i => i + 1)
+      }
     }
   }
 
@@ -40,9 +56,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={updatePersons}>
-        <div>
-          name: <input placeholder='Enter a name' value={newName} onChange={handleNewName} />
-        </div>
+        <div> name: <input placeholder='Enter a name' value={newName} onChange={handleNewName} /> </div>
+        <div> number: <input placeholder='Enter a number' value={newNumber} onChange={handleNewNumber} /></div>
         <div>
           <button type="submit">Save</button>
         </div>
